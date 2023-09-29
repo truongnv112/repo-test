753b89b3300042a7fdfdb9be41dbab0344780596
truongnv112 <truongmlpy@gmail.com>
2023-09-29 08:33:53 +0700

    cc

---
+++ CMakeFiles/model.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/model.dir/objectdetection.cpp.o"
+  "model.pdb"
+  "model"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
